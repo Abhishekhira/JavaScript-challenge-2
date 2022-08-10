28551b0f24e2de0c04f405b2f7da40f3db146ba3
Abhishek hira <92227765+Abhishekhira@users.noreply.github.com>
2022-08-10 13:49:15 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,26 @@
+// value
+// premetive string boolean number symbol bigint null undefined
+//object arrays object set map
+
+
+console.log(typeof "hiii");
+console.log(typeof 9);
+console.log(typeof true);
+console.log(typeof 2366648557857545);
+console.log(typeof undefined );
+console.log(typeof null);
+
+//strings
+"hi this is "
+//charAt()
+
+
+let firstStr="hii this is our first string";
+let inD=firstStr.charAt(10)
+console.log(inD)
+//charCodeAt()
+let ind2=firstStr.charCodeAt(10)
+console.log(ind2)
+//concat()
+//endswith
+console.log(firstStr.endsWith('abhi'))
